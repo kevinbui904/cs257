f6788cf546c6a26243613327d4ba3295f38d4675
Robbie Young <youngr2@carleton.edu>
2021-11-11 00:50:04 -0600

    Added script to clear all tables if present

--- webapp/db/clear.py
+++ webapp/db/clear.py
@@ -1,6 +1,6 @@
 '''
     convert.py
-    Author: Robbie Young
+    Authors: Kevin Bui, Robbie Young, November 11 2021
     A program designed to clear all created csv files after running convert.py
     For use in the final project in Carleton College's CS 257 Software Design Class, Fall 2021
     Credits: https://stackoverflow.com/questions/10840533/most-pythonic-way-to-delete-a-file-which-may-not-exist
