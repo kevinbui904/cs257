d95048ee0962e53a53476ae49078561ca32a2770
Thien K. M. Bui <kevinbui904@gmail.com>
2021-11-15 00:30:49 -0600

    hooked up back-end with actual front end, refactored event handler codes

--- webapp/static/getContents.js
+++ webapp/static/getContents.js
@@ -1,6 +1,6 @@
 /*getContents.js 
 Thien K. M. Bui and Robbie Young 08 November 2021
-Updated 11 November 2021
+Updated 15 November 2021
 
 
 Helper methods to fetch data from our api.py
@@ -9,17 +9,10 @@ For use in Carleton CS 251 Fall 2021
 
 const getAPIBaseUrl = () => {
 	const baseUrl = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api`;
-
 	return baseUrl;
 };
 
-// Recommend handler
-const initGetRecommended = () => {
-	const recommendButton = document.getElementById("recommend-button");
-	recommendButton.onclick = onRecommendButtonClicked;
-};
-
-const onRecommendButtonClicked = () => {
+const getRecommended = () => {
 	const genre = document.getElementById("genre-select");
 	const genreString = genre.value;
 	const url = `${getAPIBaseUrl()}/recommended?genre=${genreString}`;
@@ -31,9 +24,9 @@ const onRecommendButtonClicked = () => {
 			//internal agreement, API will ONLY ever return 1 object in a list of size 1
 			const content = jsonResponse[0];
 			contentHTML = `
-                <div id="content-container">
+                <div class="content-container">
                     <div class="content">
-                        <div id="content-title">
+                        <div class="content-title">
                             <strong>${content.title}</strong>
 							(${content.type})
                         </div>
@@ -61,161 +54,117 @@ const onRecommendButtonClicked = () => {
 };
 
 //------------------------- by title handler
-const initGetByTitle = () => {
-	const titleButton = document.getElementById("by-title-button");
-	titleButton.onclick = onTitleButtonClicked;
-};
 
-const onTitleButtonClicked = (e) => {
-	e.preventDefault();
-
-	const titleInput = document.getElementById("by-title-input");
-
-	const titleString = titleInput.value;
-	//making sure that input isn't blank
-	if (titleInput && titleString) {
-		const url = getAPIBaseUrl() + `/titles/${titleString}`;
-		// send GET request to specified URL
-		fetch(url, { method: "GET" })
-			.then((response) => response.json())
-			.then((jsonContent) => {
-				const formattedContents = jsonContent.map((content) => {
-					return `
-					<div id="content-container">
-						<div class="content">
-							<div id="content-title">
-								<strong>${content.title}</strong>
-								(${content.type})
-							</div>
-							<div class="content-subheading">
-								<strong>Director(s):</strong> ${content.directors}
-							</div>
-							<div class="content-subheading">
-								<strong>Cast(s):</strong> ${content.cast}
-							</div>
-							<div class="content-subheading">
-								<strong>Genre(s):</strong> ${content.listed_in}
-							</div>
-							<div class="content-sypnopsis">
-								<strong>Sypnopsis:</strong>
-								${content.description}
-							</div>
+const getByTitle = (titleString) => {
+	const url = getAPIBaseUrl() + `/titles/${titleString}`;
+	// send GET request to specified URL
+	fetch(url, { method: "GET" })
+		.then((response) => response.json())
+		.then((jsonContent) => {
+			const formattedContents = jsonContent.map((content) => {
+				return `
+				<div class="content-container">
+					<div class="content">
+						<div class="content-title">
+							<strong>${content.title}</strong>
+							(${content.type})
 						</div>
-					</div> 
-				`;
-				});
-				const contentsContainer = document.getElementById("contents-container");
-				contentsContainer.innerHTML = formattedContents;
-			});
-	} else {
-		window.alert("required field can not be null");
-	}
-};
-
-//------------------------- by directors handler
-const initGetByDirector = () => {
-	const directorsButton = document.getElementById("by-director-button");
-	directorsButton.onclick = onDirectorButtonClicked;
-};
-
-const onDirectorButtonClicked = (e) => {
-	e.preventDefault();
-
-	const directorInput = document.getElementById("by-director-input");
-
-	const directorString = directorInput.value;
-	//making sure that input isn't blank
-	if (directorInput && directorString) {
-		const url = getAPIBaseUrl() + `/directors/${directorString}`;
-		// send GET request to specified URL
-		fetch(url, { method: "GET" })
-			.then((response) => response.json())
-			.then((jsonContent) => {
-				const formattedContents = jsonContent.map((content) => {
-					return `
-					<div id="content-container">
-						<div class="content">
-							<div id="content-title">
-								<strong>${content.title}</strong>
-								(${content.type})
-							</div>
-							<div class="content-subheading">
-								<strong>Director(s):</strong> ${content.directors}
-							</div>
-							<div class="content-subheading">
-								<strong>Cast(s):</strong> ${content.cast}
-							</div>
-							<div class="content-subheading">
-								<strong>Genre(s):</strong> ${content.listed_in}
-							</div>
-							<div class="content-sypnopsis">
-								<strong>Sypnopsis:</strong>
-								${content.description}
-							</div>
+						<div class="content-subheading">
+							<strong>Director(s):</strong> ${content.directors}
 						</div>
-					</div> 
-				`;
-				});
-				const contentsContainer = document.getElementById("contents-container");
-				contentsContainer.innerHTML = formattedContents;
-			});
-	} else {
-		window.alert("required field can not be null");
-	}
-};
-
-//------------------------ by cast handler
-const initGetByCast = () => {
-	const titleButton = document.getElementById("by-cast-button");
-	titleButton.onclick = onCastButtonClicked;
-};
-
-const onCastButtonClicked = (e) => {
-	e.preventDefault();
-
-	const castInput = document.getElementById("by-cast-input");
-
-	const castString = castInput.value;
-	//making sure that input isn't blank
-	if (castInput && castString) {
-		const url = getAPIBaseUrl() + `/cast/${castString}`;
-		// send GET request to specified URL
-		fetch(url, { method: "GET" })
-			.then((response) => response.json())
-			.then((jsonContent) => {
-				const formattedContents = jsonContent.map((content) => {
-					return `
-					<div id="content-container">
-						<div class="content">
-							<div id="content-title">
-								<strong>${content.title}</strong>
-								(${content.type})
-							</div>
-							<div class="content-subheading">
-								<strong>Director(s):</strong> ${content.directors}
-							</div>
-							<div class="content-subheading">
-								<strong>Cast(s):</strong> ${content.cast}
-							</div>
-							<div class="content-subheading">
-								<strong>Genre(s):</strong> ${content.listed_in}
-							</div>
-							<div class="content-sypnopsis">
-								<strong>Sypnopsis:</strong>
-								${content.description}
-							</div>
+						<div class="content-subheading">
+							<strong>Cast(s):</strong> ${content.cast}
 						</div>
-					</div> 
-				`;
-				});
-				const contentsContainer = document.getElementById("contents-container");
-				contentsContainer.innerHTML = formattedContents;
+						<div class="content-subheading">
+							<strong>Genre(s):</strong> ${content.listed_in}
+						</div>
+						<div class="content-sypnopsis">
+							<strong>Sypnopsis:</strong>
+							${content.description}
+						</div>
+					</div>
+				</div> 
+			`;
 			});
-	} else {
-		window.alert("required field can not be null");
-	}
+			const contentsContainer = document.getElementById("contents-container");
+			contentsContainer.innerHTML = formattedContents;
+		});
 };
-window.addEventListener("load", initGetRecommended, true);
-window.addEventListener("load", initGetByTitle, true);
-window.addEventListener("load", initGetByDirector, true);
-window.addEventListener("load", initGetByCast, true);
+
+//------------------------- by directors
+
+const getByDirector = (directorString) => {
+	const url = getAPIBaseUrl() + `/directors/${directorString}`;
+	// send GET request to specified URL
+	fetch(url, { method: "GET" })
+		.then((response) => response.json())
+		.then((jsonContent) => {
+			const formattedContents = jsonContent.map((content) => {
+				return `
+				<div class="content-container">
+					<div class="content">
+						<div class="content-title">
+							<strong>${content.title}</strong>
+							(${content.type})
+						</div>
+						<div class="content-subheading">
+							<strong>Director(s):</strong> ${content.directors}
+						</div>
+						<div class="content-subheading">
+							<strong>Cast(s):</strong> ${content.cast}
+						</div>
+						<div class="content-subheading">
+							<strong>Genre(s):</strong> ${content.listed_in}
+						</div>
+						<div class="content-sypnopsis">
+							<strong>Sypnopsis:</strong>
+							${content.description}
+						</div>
+					</div>
+				</div> 
+			`;
+			});
+			const contentsContainer = document.getElementById("contents-container");
+			contentsContainer.innerHTML = formattedContents;
+		});
+};
+
+//------------------------ by cast
+const getByCast = (castString) => {
+	const url = getAPIBaseUrl() + `/cast/${castString}`;
+	// send GET request to specified URL
+	fetch(url, { method: "GET" })
+		.then((response) => response.json())
+		.then((jsonContent) => {
+			const formattedContents = jsonContent.map((content) => {
+				return `
+				<div class="content-container">
+					<div class="content">
+						<div class="content-title">
+							<strong>${content.title}</strong>
+							(${content.type})
+						</div>
+						<div class="content-subheading">
+							<strong>Director(s):</strong> ${content.directors}
+						</div>
+						<div class="content-subheading">
+							<strong>Cast(s):</strong> ${content.cast}
+						</div>
+						<div class="content-subheading">
+							<strong>Genre(s):</strong> ${content.listed_in}
+						</div>
+						<div class="content-sypnopsis">
+							<strong>Sypnopsis:</strong>
+							${content.description}
+						</div>
+					</div>
+				</div> 
+			`;
+			});
+			const contentsContainer = document.getElementById("contents-container");
+			contentsContainer.innerHTML = formattedContents;
+		});
+};
+
+//on window load, recommend a random content
+window.onload = getRecommended;
